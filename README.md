a9d64af627d6501dfd769e4b7900f1e559566844
sofiefe <sofiefe@stud.ntnu.no>
2025-04-14 08:30:22 +0200

    Clean up, added gitignore

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # strikkebot
 Personal project - Building a chatbot for knitting
+
+First step: A simple script to calculate increases and decreases.
+TODO: 
+[] Function to format printing of instructions.
--- calculator/increase_decrease_calculator.py
+++ calculator/increase_decrease_calculator.py
@@ -1,12 +1,12 @@
 # This will be the script for calculating increases and decreases in the round for a knitting project
 # Based on https://hobbyhygge.com/nb/strikkekalkulator
 
-# This script will have some main functions:
 # The first function takes two inputs; current amount of stiches, and wanted amount of stiches
 # It will return the instructions to increase/decrease
 # The instructions will have the format:
 # [[knitXstiches, Y=-1 for decrease or Y=1 for increase, repeatZtimes]]
 # the list can contains multiple instructions.
+
 # Second function wil print a step-by-step interactive guide to help keeping track of the decreases
 
 def calculateIncDec(currentStiches, wantedStitches):
