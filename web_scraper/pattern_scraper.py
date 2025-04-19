aef7fc4b124b308ce6c1377d19e7edc9d18959fb
sofiefe <sofiefe@stud.ntnu.no>
2025-04-19 10:31:49 +0200

    Can now retrieve all pattern links

--- web_scraper/pattern_scraper.py
+++ web_scraper/pattern_scraper.py
@@ -1,8 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
 from bs4 import BeautifulSoup
 
 def pattern_scraper(URL):
--- web_scraper/scraper.py
+++ web_scraper/scraper.py
@@ -1,15 +1,53 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
+from page_scraper import page_scraper, load_page, write_links
 from bs4 import BeautifulSoup
+import time
 
-URL = ""
+URL = "https://www.garnstudio.com/search.php?action=browse&mt=1&page=1&sort=date&lang=no"
+
+# function to get the last page
+
+# Iterate trough each page using page_scraper() and save pattern info
+# Use the links to scrape patterns using pattern_scraper
+# Save data in a fitting format
+
+def find_last_page(soup):
+    page_div = soup.find_all("li", class_="page-item")
+    last_page_button = page_div[-1].find("a", href=True)
+    link = last_page_button["href"] if last_page_button else "No link"
+    last_page = link.strip("/search.php?action=browse&mt=1&page=").split("&")[0]
+    return int(last_page)
+
 
 def scraper():
-    pass
+    driver = webdriver.Firefox()
+    driver.get(URL)
 
-    # Iterate trough each page using page_scraper() and save pattern info
-    # Use the links to scrape patterns using pattern_scraper
-    # Save data in a fitting format
+    try:
+        load_page(driver)
+        soup = BeautifulSoup(driver.page_source, "html.parser")
+        last_page = find_last_page(soup)
+
+        pattern_info = []
+
+        base_url = "https://www.garnstudio.com/search.php?action=browse&mt=1&page={}&sort=date&lang=no"
+
+        for page in range(1, last_page+1):
+            print(f"Getting links from page {page}")
+            page_url = base_url.format(page)
+            pattern_info.append(page_scraper(page_url))
+        
+        write_links(pattern_info)
+
+
+    except Exception as e:
+        print("No patterns found:", e)
+
+    driver.quit()
+
+
+if __name__ == "__main__":
+    print("Start getting links")
+    start_time = time.time()
+    scraper()
+    print(f"Getting links took {time.time()-start_time:.2f}")
